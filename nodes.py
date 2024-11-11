49677ee587a28457a2470d3377544bf3136d06e6
Anze- <alberto.anzellotti@gmail.com>
2024-11-11 10:45:02 +0100

    Dummy nodes definition and mapping

---
+++ nodes.py
@@ -0,0 +1,19 @@
+# ComnfyUI: Node definitions
+def deepDebandModelLoader():
+    ...
+
+def deepDebandInference():
+    ...
+
+
+
+# ComfyUI: Node class mappings
+NODE_CLASS_MAPPINGS = {
+  "deepDeband Model Loader": deepDebandModelLoader,
+  "deepDeband Inference": deepDebandInference,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+  "deepDebandModelLoader": "deepDeband Model Loader",
+  "deepDebandInference": "deepDeband Inference",
+}
